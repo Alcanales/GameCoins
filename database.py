c68235e5d4a75235b08aa7c7109fe72d070e5ab1
Alejandro Canales <101652998+Alcanales@users.noreply.github.com>
2025-12-11 20:25:01 -0300

    Update database.py

--- database.py
+++ database.py
@@ -5,8 +5,14 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if DATABASE_URL:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+ 
+    if "sslmode=" not in DATABASE_URL and "localhost" not in DATABASE_URL and "127.0.0.1" not in DATABASE_URL:
+        separator = "&" if "?" in DATABASE_URL else "?"
+        DATABASE_URL += f"{separator}sslmode=require"
 
 engine = create_engine(DATABASE_URL if DATABASE_URL else "sqlite:///./local.db")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
