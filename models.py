92c7c65aa24a2b780fd0cbda9019bf941e284916
Alejandro Canales <101652998+Alcanales@users.noreply.github.com>
2025-12-12 19:07:42 -0300

    Update GameCoinUser model with new fields and constraints

--- models.py
+++ models.py
@@ -6,8 +6,9 @@ class GameCoinUser(Base):
     __tablename__ = "gamecoins"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    name = Column(String, default="")
-    rut = Column(String, default="")
-    saldo = Column(Integer, default=0)
+    email = Column(String, unique=True, index=True, nullable=False)
+    rut = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False, default="Cliente")
+    surname = Column(String, nullable=False, default="")
+    saldo = Column(Integer, default=0, nullable=False)
     updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
