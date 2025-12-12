06fb3065301d2a07e2742a2612b0dccfe61c5fa8
Alejandro Canales <101652998+Alcanales@users.noreply.github.com>
2025-12-12 12:56:28 -0300

    Add name and rut fields to GameCoinUser model
    Added 'name' and 'rut' fields with default values to GameCoinUser model.

--- models.py
+++ models.py
@@ -2,10 +2,16 @@ from sqlalchemy import Column, Integer, String, DateTime
 from database import Base
 import datetime
 
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+import datetime
+
 class GameCoinUser(Base):
     __tablename__ = "gamecoins"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+    name = Column(String, default="")
+    rut = Column(String, default="")
     saldo = Column(Integer, default=0)
     updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
