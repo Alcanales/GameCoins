31efeaa39b0c30084c29cddc9cfe3a4aee6bb5ea
Alejandro Canales <101652998+Alcanales@users.noreply.github.com>
2026-01-30 13:39:09 -0300

    Update models.py

--- models.py
+++ models.py
@@ -2,11 +2,19 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from database import Base
 
+# 1. Tabla de Usuarios (La que ya tenías)
 class GameCoinUser(Base):
     __tablename__ = "gamecoins"
+    
     email = Column(String, primary_key=True, index=True)
     rut = Column(String, nullable=True)
     name = Column(String, nullable=True)
     saldo = Column(Integer, default=0)
     historico_canjeado = Column(Integer, default=0)
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), default=func.now())
+
+class SystemConfig(Base):
+    __tablename__ = "system_config"
+    
+    key = Column(String, primary_key=True, index=True) 
+    value = Column(String)                            
