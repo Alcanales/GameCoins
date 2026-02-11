61c48c0a113ac18820011d2012fdf045a526e8b2
Alejandro Canales <101652998+Alcanales@users.noreply.github.com>
2026-02-11 20:05:34 -0300

    Refactor GameCoinUser and SystemConfig table

--- GameCoins/models.py
+++ GameCoins/models.py
@@ -1,15 +1,17 @@
-from sqlalchemy import Column, String, Integer
-from .database import Base  # Import relativo corregido
+from sqlalchemy import Column, Integer, String
+from .database import Base
 
 class GameCoinUser(Base):
-    __tablename__ = "gamecoins"
-    email = Column(String, primary_key=True, index=True)
+    __tablename__ = "game_coin_users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    saldo = Column(Integer, default=0)
     name = Column(String, nullable=True)
     surname = Column(String, nullable=True)
-    saldo = Column(Integer, default=0, index=True)
     historico_canjeado = Column(Integer, default=0)
 
 class SystemConfig(Base):
-    __tablename__ = "system_config"
+    __tablename__ = "system_configs"
     key = Column(String, primary_key=True, index=True)
     value = Column(String)
