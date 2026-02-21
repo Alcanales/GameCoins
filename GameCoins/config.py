13270282aa617f1674780adc21662ae7d85e1268
Alejandro Canales <101652998+Alcanales@users.noreply.github.com>
2026-02-21 13:35:26 -0300

    Refactor config.py to remove commented lines

--- GameCoins/config.py
+++ GameCoins/config.py
@@ -15,13 +15,12 @@ class Settings(BaseSettings):
     ADMIN_PASS: str = os.getenv("ADMIN_PASS", "change_me")
     STORE_TOKEN: str = os.getenv("STORE_TOKEN")
     
-    # Credenciales Jumpseller (Renombradas para evitar confusión)
     JS_LOGIN_CODE: str = os.getenv("JS_LOGIN_CODE", "")
     JS_AUTH_TOKEN: str = os.getenv("JS_AUTH_TOKEN", "")
     JUMPSELLER_API_BASE: str = "https://api.jumpseller.com/v1"
 
     USD_TO_CLP: int = int(os.getenv("USD_TO_CLP", 1000))
-    MIN_CANJE: int = int(os.getenv("MIN_CANJE", 100)) # Mínimo $100
+    MIN_CANJE: int = int(os.getenv("MIN_CANJE", 100))
     MIN_PURCHASE_USD: float = float(os.getenv("MIN_PURCHASE_USD", 3.0))
     MAINTENANCE_MODE_CANJE: bool = str(os.getenv("MAINTENANCE_MODE_CANJE", "false")).lower() == "true"
     
