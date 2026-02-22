93a3c324b02dea2e89702aeceab22737aa0a3370
Alejandro Canales <101652998+Alcanales@users.noreply.github.com>
2026-02-22 18:14:33 -0300

    Update database.py

--- GameCoins/database.py
+++ GameCoins/database.py
@@ -1,13 +1,20 @@
+# GameCoins/database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL, 
+    pool_pre_ping=True,
+    connect_args={"options": "-csearch_path=public"} 
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
